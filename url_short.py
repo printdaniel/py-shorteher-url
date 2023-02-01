883ffe38bf60d9ba337b81797e0d5446dd5dc2ae
Daniel Suárez <dwsuarez98@gmail.com>
2023-01-31 22:09:27 -0300

    identation

--- url_short.py
+++ url_short.py
@@ -1,12 +1,12 @@
 import pyshorteners
 
-
-
 def shorten(url):
     link = pyshorteners.Shortener()
     return link.tinyurl.short(url)
 
-
 if __name__ == "__main__":
     url = input("Ingrese el link que desea acortar: ")
     print(f"\n{shorten(url)}")
+
+
+    
